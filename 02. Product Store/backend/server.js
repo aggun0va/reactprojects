25b2db0313e0fa90aa688a25d5c93c3e4824ddff
Agu <aggun0va@gmail.com>
2026-06-13 12:37:24 +0400

    config db

--- 02. Product Store/backend/server.js
+++ 02. Product Store/backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import { connectDB } from './config/db.js';
 // import cors from "cors";
 // import path from 'path';
 // import { fileURLToPath } from 'url';
@@ -8,10 +9,19 @@ import dotenv from 'dotenv';
 
 dotenv.config();
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 // app.use(cors()); 
+// console.log(process.env.MONGO_URI);
+
+app.get('/', (req, res) => {
+  res.send('Hello, World!');
+});
+
+
+app.get('/api/products', (req, res) => {});
 
 app.listen(port, () => {
+  connectDB();
   console.log(`Server is running on port 'http://localhost:${port}'`);
 });
